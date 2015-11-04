24d54b87c18306888f29cb7d349b9238346a57ce
Vladimir <vherever@gmail.com>
2015-11-05 00:17:44 +0300

    Player shooting action no bullets

---
+++ Assets/_Scripts/DamageHandler.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DamageHandler : MonoBehaviour {
+
+	public int health = 1;
+
+	float invulnTimer = 0;
+	int correctLayer;
+
+	void Start() {
+		correctLayer = gameObject.layer;
+	}
+
+	void OnTriggerEnter2D() {
+		Debug.Log ("Trigger!");
+
+		health --;
+		invulnTimer = 0.25f;
+		gameObject.layer = 10;
+	}
+
+	void Update() {
+		invulnTimer -= Time.deltaTime;
+		if (invulnTimer <= 0) {
+			gameObject.layer = correctLayer;
+		}
+
+		if (health <= 0) {
+			Die();
+		}
+	}
+
+	void Die() {
+		Destroy(gameObject);
+	}
+}
---
+++ Assets/_Scripts/PlayerShooting.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerShooting : MonoBehaviour {
+
+	public float fireDelay = 0.25f;
+	float cooldownTimer = 0;
+			
+	// Update is called once per frame
+	void Update () {
+		cooldownTimer -= Time.deltaTime;
+
+		if (Input.GetButtonDown ("Fire1") && cooldownTimer <= 0) {
+			cooldownTimer = fireDelay;
+			print("Shoot!");
+		}
+	}
+}
