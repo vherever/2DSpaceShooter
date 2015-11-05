d9444fe97f8183e12a1ac31abcef6b547cdfdd8e
Vladimir <vromanyuk@gmail.com>
2015-11-05 18:37:17 +0200

    Enemy spawning

--- Assets/_Scripts/EnemyShooting.cs
+++ Assets/_Scripts/EnemyShooting.cs
@@ -3,13 +3,16 @@ using System.Collections;
 
 public class EnemyShooting : MonoBehaviour {
 
-    public Vector3 bulletOffset = new Vector3(0, 0.8f, 0);
+    public Vector3 bulletOffset = new Vector3(0, 0.5f, 0);
 
     public GameObject bulletPrefab;
     int bulletLayer;
 
     public float fireDelay = 0.5f;
     float cooldownTimer = 0;
+
+    Transform player;
+
 
     void Start() {
         bulletLayer = gameObject.layer;
@@ -18,9 +21,21 @@ public class EnemyShooting : MonoBehaviour {
     // Update is called once per frame
     void Update()
     {
+
+        if (player == null)
+        {
+            // Find the player's ship!
+            GameObject go = GameObject.FindWithTag("Player");
+
+            if (go != null)
+            {
+                player = go.transform;
+            }
+        }
+
         cooldownTimer -= Time.deltaTime;
 
-        if (cooldownTimer <= 0)
+        if (cooldownTimer <= 0 && player != null && Vector3.Distance(transform.position, player.position) < 4)
         {
             cooldownTimer = fireDelay;
 
